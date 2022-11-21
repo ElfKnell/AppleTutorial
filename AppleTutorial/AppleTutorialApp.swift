1fa9f7ccfb25279c1554d9bb53e0322393d103ac
Andrii Kyrychenko <akyrych84@gmail.com>
2022-11-21 19:07:13 -0400

    Initial Commit

---
+++ AppleTutorial/AppleTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTutorialApp.swift
+//  AppleTutorial
+//
+//  Created by Andrii Kyrychenko on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
