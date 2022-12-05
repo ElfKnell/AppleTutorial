c7f4d4b3f7f5e8a02190c5b259bb51ab4c05b4c4
Andrii Kyrychenko <akyrych84@gmail.com>
2022-12-04 20:27:54 -0400

    End Watch App

--- WatchLandmarks Watch App/WatchLandmarksApp.swift
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -5,13 +5,20 @@
 //  Created by Andrii Kyrychenko on 04/12/2022.
 //
 
-//import SwiftUI
-//
-////@main
-//struct WatchLandmarks_Watch_AppApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
-//}
+import SwiftUI
+
+//@main
+struct WatchLandmarks_Watch_AppApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+    }
+}
